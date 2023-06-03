60e1103a69e93728ee9c7bd4ae2b406d825961af
Ademir Colares Dos Santos Junior <ademircsj@gmail.com>
2023-06-03 20:05:27 -0300

    codificadas funcionalidades do formulário de interesses

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const campoPrioridade = document.querySelector("#campoPrioridade");
 const valorPrioridade = document.querySelector("#valorPrioridade");
 const btnInteressesLimpar = document.querySelector("#limpar");
 const btnInteressesAdicionar = document.querySelector("#adicionar");
+const tabelaInteresses = document.querySelector("#tabelaInteresses");
 // fim da entrada de dados do formulario prioridades
 
 
@@ -50,16 +51,61 @@ function limparCampos(form){
 
 }
 
+function criarLinhaInteresse(){//cria para o formulario de interesses uma linha e captura os valores inseridos
+  let linha = document.createElement('tr');
+  linha.setAttribute("class","linha");
+
+  let interesse = document.createElement('td');
+  let prioridade = document.createElement('td');
+  let linhaIconExcluir = document.createElement('td');
+  let iconClass = document.createElement('i');
+  iconClass.setAttribute("class","fa-solid fa-trash");
+  linhaIconExcluir.appendChild(iconClass);
+
+  interesse.innerText = campoInteresse.value;
+  prioridade.innerText = campoPrioridade.value;
+
+
+
+  linha.appendChild(interesse);
+  linha.appendChild(prioridade);
+  linha.appendChild(linhaIconExcluir);
+
+  // console.log(linha);
+
+    return (linha);
+}
+
+
+function adicionarInteresse(){
+
+  let linha = criarLinhaInteresse();
+  let contInteresses = 0;
+  tabelaInteresses.appendChild(linha);
+  console.log(linha);
+  console.log(tabelaInteresses);
+  contInteresses += 1;
+
+  return(contInteresses);
+
+}
 
 
 
 //Fluxo Principal
 
-mostraPrioridade(campoPrioridade, valorPrioridade); //Mostra e atualiza o número de prioridade do interesse.
+mostraPrioridade(campoPrioridade, valorPrioridade); //Mostra e atualiza o número de prioridade do interesse conforme seleção do user.
 
-campoPrioridade.addEventListener('input', ()=>{mostraPrioridade(campoPrioridade, valorPrioridade)});
+campoPrioridade.addEventListener('input', ()=>{mostraPrioridade(campoPrioridade, valorPrioridade)}); // adiciona o evento input para o seletor range
 
-btnInteressesLimpar.addEventListener("click",function(e){
-  limparCampos(formInteresses)
-  e.preventDefault()
+btnInteressesLimpar.addEventListener("click",function(e){ //adiciona o evento click ao botão limpar
+  limparCampos(formInteresses);
+  e.preventDefault();
 });
+
+ contInteresses = btnInteressesAdicionar.addEventListener('click', function(e){
+  adicionarInteresse();
+  e.preventDefault();
+})
+
+// console.log(contInteresses);
