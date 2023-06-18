65df7fec5dfd0e680d69bf1f5a5fa4899ccc9330
Ademir Colares Dos Santos Junior <ademircsj@gmail.com>
2023-06-17 23:04:23 -0300

    criada logica para salvar no ls

--- src/jsScripts/cadVolunt.js
+++ src/jsScripts/cadVolunt.js
@@ -1,59 +1,118 @@
-const formInteresses = document.querySelector("#formInteresses");
 const formInformacoes = document.querySelector("#formInfBasica");
+const formExperiencias = document.querySelector("#experiencias");
 const formQualificacoes = document.querySelector("#qualificacoes");
-const interesse = document.querySelector("#campoInteresse");
-const prioridade = document.querySelector("#campoPrioridade");
-const btnSalvar = document.querySelector("#salvarUser");
+const btnSalvarUser = document.querySelector("#salvarUser");
+const btnSalvarXp = document.querySelector("#btnSalvar");
+const btnSalvarQualif = document.querySelector("#btnSalvarCurso");
+const listaQualificacoes = document.querySelector("#listaQualificacoes");
 
+const inicioCurso = document.querySelector("#inicioCurso");
+const fimCurso = document.querySelector("#fimCurso");
+const nomeInstituicao = document.querySelector("#nomeInstituicao");
+const areaCurso = document.querySelector("#areaCurso");
+const nomeCurso = document.querySelector("#nomeCurso");
+const tipoDeCurso = document.querySelector("#tipoCurso");
 
-
-
-
-
-function salvarFormulario(formulario) {
-  // Captura os valores já existentes no localStorage
-  var dadosExistentes = localStorage.getItem('dadosFormulario');
-
-  // Verifica se existem dados no localStorage
-  var dados = dadosExistentes ? JSON.parse(dadosExistentes) : [];
-
-  // Objeto para armazenar os valores capturados do formulário
-  var valores = {};
-
-  // Captura todos os elementos de input e textarea do formulário
-  var inputs = formulario.getElementsByTagName('input');
-  var textareas = formulario.getElementsByTagName('textarea');
-
-  // Itera sobre os inputs e armazena os valores no objeto
-  for (var i = 0; i < inputs.length; i++) {
-    var input = inputs[i];
-    valores[input.name] = input.value;
-  }
-
-  // Itera sobre as textareas e armazena os valores no objeto
-  for (var j = 0; j < textareas.length; j++) {
-    var textarea = textareas[j];
-    valores[textarea.name] = textarea.value;
-  }
-
-  // Adiciona o objeto de valores ao array de dados
-  dados.push(valores);
-
-  // Converte o array de dados em uma string JSON
-  var dadosJSON = JSON.stringify(dados);
-
-  // Armazena a string JSON no localStorage
-  localStorage.setItem('dadosFormulario', dadosJSON);
+function converteDataParaPadraoBrasileiro(data) {
+  let dataBrasileira;
+  dataBrasileira = data.split('-').reverse().join("/");
+  return dataBrasileira;
 }
 
+function criarCardQualificacao(dados) {
+  var cardDetalhes = document.createElement('div');
+  cardDetalhes.className = 'campoDetalhe cardExp';
+  cardDetalhes.id = 'cardCurso';
+
+  var listaUl = document.createElement('ul');
+  var dataInicioLi = document.createElement('li');
+  dataInicioLi.textContent = 'de: ' + converteDataParaPadraoBrasileiro(dados.inicioCurso);
+  var dataFimLi = document.createElement('li');
+  dataFimLi.textContent = 'até: ' + converteDataParaPadraoBrasileiro(dados.fimCurso);
+  var instituicaoLi = document.createElement('li');
+  instituicaoLi.textContent = 'instituição: ' + dados.nomeInstituicao;
+  var nomeCursoLi = document.createElement('li');
+  nomeCursoLi.textContent = dados.nomeCurso;
+  var tipoDeCursoLi = document.createElement('li');
+  tipoDeCursoLi.textContent = 'tipo de curso: ' + dados.tipoCurso;
+  var areaCursoLi = document.createElement('li');
+  areaCursoLi.textContent = 'área do curso: ' + dados.areaCurso;
+  var tituloCard = document.createElement('h2');
+  tituloCard.textContent = dados.nomeCurso;
+  tituloCard.setAttribute("class", "titulosH3");
 
 
-//SALVA NO LOCALSTORAGE OS DADOS DO FORMULÀRIO DE INFORMAÇÕES
-btnSalvar.addEventListener("click", function(e){
+  listaUl.appendChild(dataInicioLi);
+  listaUl.appendChild(dataFimLi);
+  listaUl.appendChild(instituicaoLi);
+  listaUl.appendChild(tipoDeCursoLi);
+  listaUl.appendChild(areaCursoLi);
 
-     salvarFormulario(formInformacoes);
+  var divGroupBtn = document.createElement('div');
+  divGroupBtn.className = 'groupBtn btnArea';
+  divGroupBtn.id = 'btnArea';
 
+  var botaoEditar = document.createElement('button');
+  botaoEditar.type = 'submit';
+  botaoEditar.className = 'formBtn';
+  botaoEditar.textContent = 'editar';
 
-    e.preventDefault();
+  var botaoExcluir = document.createElement('button');
+  botaoExcluir.type = 'submit';
+  botaoExcluir.className = 'formBtn';
+  botaoExcluir.textContent = 'excluir';
 
-})
+  divGroupBtn.appendChild(botaoEditar);
+  divGroupBtn.appendChild(botaoExcluir);
+
+  cardDetalhes.appendChild(tituloCard);
+  cardDetalhes.appendChild(listaUl);
+  cardDetalhes.appendChild(divGroupBtn);
+
+  return cardDetalhes;
+}
+
+function recuperarDados(formId) {
+  var dados = localStorage.getItem(formId);
+
+  if (dados) {
+    return JSON.parse(dados);
+  } else {
+    return [];
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function (e) {
+  var dados = recuperarDados('form3'); // Recupera os dados do localStorage
+
+  for (var i = 0; i < dados.length; i++) {
+    var card = criarCardQualificacao(dados[i]);
+    listaQualificacoes.appendChild(card);
+  }
+
+  e.preventDefault();
+});
+
+btnSalvarQualif.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  var dados = recuperarDados('form3'); // Recupera os dados do localStorage
+
+  var qualificacao = {
+    inicioCurso: inicioCurso.value,
+    fimCurso: fimCurso.value,
+    nomeInstituicao: nomeInstituicao.value,
+    nomeCurso: nomeCurso.value,
+    tipoCurso: tipoDeCurso.value,
+    areaCurso: areaCurso.value
+  };
+
+  dados.push(qualificacao);
+
+  localStorage.setItem('form3', JSON.stringify(dados));
+
+  var card = criarCardQualificacao(qualificacao);
+  listaQualificacoes.appendChild(card);
+
+  formQualificacoes.reset();
+});
