169443bb34e01b6eaba199e82f4b9f12717fd311
anabmilk <anabmilk@gmail.com>
2023-06-20 15:54:43 -0300

    Pesquisa de vagas implementada

--- src/componentes/navbar-script.js
+++ src/componentes/navbar-script.js
@@ -34,51 +34,9 @@ searchButton.addEventListener("click", () => {
 
   //displayResults(results);
 
-    // Armazena os resultados no localStorage
-    localStorage.setItem('searchResults', JSON.stringify(results));
+  // Armazena os resultados no localStorage
+  localStorage.setItem("searchResults", JSON.stringify(results));
 
-    // Redireciona para a página de resultados
-    window.location.href = 'paginaResultado.html';
+  // Redireciona para a página de resultados
+  window.location.href = "paginaResultado.html";
 });
-
-/*function displayResults(results) {
-  searchResultContainer.innerHTML = "";
-
-  if (results.length === 0) {
-    searchResultContainer.textContent = "Nenhum resultado encontrado";
-    return;
-  }
-
-  results.forEach((ong) => {
-    console.log(ong);
-    const resultItem = document.createElement("div");
-    resultItem.classList.add("searchResultItem");
-
-    const img = document.createElement("img");
-    img.src = ong.fotoPerfil;
-    img.alt = "Foto de perfil da ONG";
-
-    const name = document.createElement("h3");
-    name.textContent =
-      ong.nomeUsuarioONG !== undefined ? ong.nomeUsuarioONG : ong.nomeUsuario;
-
-    const biografia = document.createElement("p");
-    biografia.textContent = ong.biografia;
-
-    const interesses = document.createElement("p");
-    interesses.textContent = ong.interesses;
-
-    const verMaisButton = document.createElement("button");
-    verMaisButton.textContent = "Ver mais";
-    verMaisButton.addEventListener("click", () => {
-      window.location.href =
-        ong.nomeUsuarioONG !== undefined
-          ? `paginaPerfilONG.html?id=${ong.id}`
-          : `paginaPerfilVol.html?id=${ong.id}`;
-    });
-
-    resultItem.append(img, name, biografia, interesses, verMaisButton);
-
-    searchResultContainer.appendChild(resultItem);
-  });
-};*/
